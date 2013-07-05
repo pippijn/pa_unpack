35a6216b05bee48d9db2206fe1a04184d6dc01ce
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-07-05 13:33:06 +0200

    use mlmake preprocessor

---
+++ src/unpack/OMakefile.ml
@@ -0,0 +1,12 @@
+install Library ".DEFAULT" [
+  (* Target *)
+  Name		"unpack";
+  Description	"Unpack support library";
+  Version	"0.1";
+
+  (* Sources *)
+  Modules [
+    "Bitstream";
+    "Unpack";
+  ];
+]
